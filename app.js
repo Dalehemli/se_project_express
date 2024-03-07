2e8017c5341813e3d19470bb8324293854bfed4f
Dale Hemli <hemlidale@gmail.com>
2024-03-07 17:32:16 -0500

    Ireallyhavenoclueatm

--- app.js
+++ app.js
@@ -17,10 +17,17 @@ const routes = require("./routes");
 app.use(express.json());
 app.use(cors());
 app.use(requestLogger);
+
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+//sorry if this is a mess, I'm sort of lost. Thanks for any help in advance...
 app.use(routes);
 app.use(errorLogger);
-// app.use(errors());
-// app.use(errorHandler);
+app.use(errors());
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`App is listening at port ${PORT}`);
