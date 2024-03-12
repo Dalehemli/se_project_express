841a655d483a62fa08d99607d04e4320562d147a
Dale Hemli <hemlidale@gmail.com>
2024-03-12 13:34:22 -0400

    needserverconfig

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -28,6 +28,7 @@ const validateAvatar = celebrate({
 });
 
 //I think this is what you were asking for with the validation functions for name and avatar?
+//Also, I just have to figure out the server configuration and where I went wrong. Just wanted to make sure got the corrections done
 
 const validateCreatedItem = celebrate({
   body: Joi.object().keys({
